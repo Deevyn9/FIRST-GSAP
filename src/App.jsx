7a7894960c007a527500a9e37b0bd32bf65dd686
Divine Obeten <divineobeten9@gmail.com>
2023-04-03 18:19:20 +0100

    created circle classes and changed background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import React, { useEffect, useRef } from 'react'
+import { Power3 } from 'gsap/gsap-core'
 
 function App() {
+
   return (
     <div className="App">
- 
+      <header className="App-header">
+        <div className="circle-container">
+          <div className="circle"></div>
+          <div className="circle red"></div>
+          <div className="circle blue"></div>
+        </div>
+      </header>
     </div>
   )
 }
